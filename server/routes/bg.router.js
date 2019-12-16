a30b773296c65d0480e2f4599b2673a5b013a2b7
Trevor Ramlow <trevorramlow@gmail.com>
2019-12-16 12:44:28 -0600

    a user can type a game and search the boardgame atlas api and it will return the results in the console

--- server/routes/bg.router.js
+++ server/routes/bg.router.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const axios = require('axios');
+
 
 /**
  * GET route template
  */
 router.get('/:searchQuery', (req, res) => {
-    res.send('bg router get hit')
+    // console.log('req.params.searchQuery:', req.params.searchQuery)
+    axios.get(`https://www.boardgameatlas.com/api/search?name=${req.params.searchQuery}&client_id=${process.env.bga_client_id}`).then(response => {
+        // console.log('bg router get search response:', response.data)
+        res.send(response.data)
+    })
+
 });
 
 /**
--- src/redux/sagas/boardgameSaga.js
+++ src/redux/sagas/boardgameSaga.js
@@ -1,15 +1,11 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
-require('dotenv').config();
-
-
 function* bgSearch(action) {
   try {
    yield console.log('bgSearch saga hit. payload:', action.payload)
-//    const bgResponse = yield axios.get(`https://www.boardgameatlas.com/api/search?name=${action.payload}&client_id=9efyAUKWxl`);
    const bgResponse = yield axios.get(`api/boardgames/${action.payload}`);
-   yield console.log('boardgame search response:', bgResponse)
+   yield console.log('boardgame search response:', bgResponse.data)
   } catch (error) {
       console.log('Error with bgSearchSaga:', error);
   }
