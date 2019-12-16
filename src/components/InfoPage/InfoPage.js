848870a98f877a49aa38b094ffb87f6dbc56caa1
Trevor Ramlow <trevorramlow@gmail.com>
2019-12-16 11:16:23 -0600

    starting working on setting up page to search boardgame database

--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
-const InfoPage = () => (
-  <div>
-    <p>
-      Info Page
-    </p>
-  </div>
-);
+class InfoPage extends Component {
+
+  componentDidMount() {
+    console.log('Component did Mount')
+  }
+
+  handleChange = (event) => {
+    console.log("input:", event.target.value)
+  }
+
+  render() {
+    return (
+      <div>
+        <label>Board Game Title:</label>
+        <form>
+          <input onChange={this.handleChange}></input><input type="button"></input>
+        </form>
+      </div>
+    );
+  }
+
+}
 
 export default InfoPage;
