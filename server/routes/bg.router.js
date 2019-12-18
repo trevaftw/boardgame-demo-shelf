c9d207d35e42d917267b55f8c0281225a9b50963
Trevor Ramlow <trevorramlow@gmail.com>
2019-12-18 11:46:27 -0600

    add to shelf now updates database. next add skill level selecter before adding to database

--- server/routes/bg.router.js
+++ server/routes/bg.router.js
@@ -15,7 +15,41 @@ router.get('/search/:searchQuery', (req, res) => {
 });
 
 router.post('/add', (req, res) => {
-    console.log('bg add post route:', req.body)
+    // console.log('bg add post route:', req.body)
+    pool.query(`
+    INSERT INTO "shelf" (
+        "bg_id",
+        "name", 
+        "min_players",
+        "max_players",
+        "min_playtime",
+        "max_playtime",
+        "description_preview", 
+        "image_url",
+        "official_url",
+        "rules_url",
+        "skill_level"
+    )
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);`,
+        [
+            req.body.id,
+            req.body.name,
+            req.body.min_players,
+            req.body.max_players,
+            req.body.min_playtime,
+            req.body.max_playtime,
+            req.body.description_preview,
+            req.body.image_url,
+            req.body.official_url,
+            req.body.rules_url,
+            req.body.skill_level
+        ]
+    ).then(() => {
+        res.sendStatus(200)
+    }).catch(error => {
+        console.log('error with add to demo shelf router post:', error)
+        res.sendStatus(500)
+    })
 });
 
 module.exports = router;
