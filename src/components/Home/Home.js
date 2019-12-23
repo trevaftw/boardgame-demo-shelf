c308ae398a04bcea70c34e17844ff994454355cf
Trevor Ramlow <trevorramlow@gmail.com>
2019-12-23 13:56:07 -0600

    basic structure setup for the demo shelf:

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+//styling
+import './Home.css'
+
 class DemoShelf extends Component {
 
   componentDidMount() {
@@ -13,8 +16,30 @@ class DemoShelf extends Component {
 
   render() {
     return (
-      <div>
-        This page will display the boardgames
+      <div className="homeDiv">
+        <div className="homeBody">
+          How many players?<br />
+          <select>
+            <option>(select)</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6+</option>
+          </select><br /><br />
+          Skill Level? <br />
+          <select>
+            <option value="null">(select)</option>
+            <option value="Beginner">Beginner</option>
+            <option value="Easy">Easy</option>
+            <option value="Intermediate">Intermediate</option>
+            <option value="Advanced">Advanced</option>
+          </select>
+        </div>
+        <div className="homeResults">
+          {/* map the results into a component displaying them as a list */}
+        </div>
       </div>
     );
   }
