1d841e7e117e5d7f341dd9e4f84d03b5a04f6750
Trevor Ramlow <trevorramlow@gmail.com>
2019-12-27 14:08:01 -0600

    changed home component route from home to empty

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -30,13 +30,9 @@ class App extends Component {
         <div>
           <Nav />
           <Switch>
-            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-            <Redirect exact from="/" to="/home" />
-            {/* Visiting localhost:3000/about will show the about page.
-            This is a route anyone can see, no login necessary */}
             <Route
               exact
-              path="/home"
+              path="/"
               component={Home}
             />
             {/* For protected routes, the view could show one of several things on the same route.
